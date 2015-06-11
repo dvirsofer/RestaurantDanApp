a1d1a4d150a10f2d1658238f8739c7ee4dfc6437
dvirsofer <dvir.sofer90@gmail.com>
2015-06-11 04:03:56 -0700

    Initial Commit

---
+++ RestaurantDanApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RestaurantDanApp
+//
+//  Created by DvirSofer on 6/11/15.
+//  Copyright (c) 2015 DvirSofer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
