c12f11737be6c6935fcd8fc2e494b6db6702fb69
dvirsofer <dvir.sofer90@gmail.com>
2015-06-11 04:53:03 -0700

    test

---
+++ RestaurantDanApp/food.h
@@ -0,0 +1,13 @@
+//
+//  food.h
+//  RestaurantDanApp
+//
+//  Created by DvirSofer on 6/11/15.
+//  Copyright (c) 2015 DvirSofer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface food : NSObject
+
+@end
