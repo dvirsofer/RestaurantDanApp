acef01b38f5e0d3e4e23e2fa589a60d824913997
ormed <ormed1994@gmail.com>
2015-06-15 10:55:35 -0700

    Added directories and Login page

---
+++ RestaurantDanApp/Controllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  RestaurantDanApp
+//
+//  Created by Or on 6/12/15.
+//  Copyright (c) 2015 DvirSofer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ RestaurantDanApp/Controllers/LoginViewController.m
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.m
+//  RestaurantDanApp
+//
+//  Created by Or on 6/12/15.
+//  Copyright (c) 2015 DvirSofer. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@implementation LoginViewController
+
+@end
