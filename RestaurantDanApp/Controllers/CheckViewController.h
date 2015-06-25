7378d0940b240154fc7bd795ff6c5d4beb056b00
ormed <ormed1994@gmail.com>
2015-06-25 15:42:59 -0700

    Changes in storyboard - no fix tableView yet

---
+++ RestaurantDanApp/Controllers/CheckViewController.h
@@ -0,0 +1,13 @@
+//
+//  CheckViewController.h
+//  RestaurantDanApp
+//
+//  Created by Or on 6/25/15.
+//  Copyright (c) 2015 DvirSofer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CheckViewController : UIViewController
+
+@end
---
+++ RestaurantDanApp/Controllers/CheckViewController.m
@@ -0,0 +1,27 @@
+//
+//  CheckViewController.m
+//  RestaurantDanApp
+//
+//  Created by Or on 6/25/15.
+//  Copyright (c) 2015 DvirSofer. All rights reserved.
+//
+
+#import "CheckViewController.h"
+
+@interface CheckViewController ()
+
+@end
+
+@implementation CheckViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
